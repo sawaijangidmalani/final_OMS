8ac32784ed71ea661f6755a6a827da92c64ef4b0
Sawai jangid <sawaisingh0706@gmail.com>
2024-09-26 11:44:38 +0530

    commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -12,10 +12,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      // Ensure this is not excluding 'react-dom/client'
-      external: [],
-    },
-  },
 });
+
